53a4d6abe3b12747b4cd8eaa375e7a75713a0c21
Ewen <ewen.herve.35550@gmail.com>
2022-10-14 09:40:28 +0200

    refactor: script database

--- src/database/database.sql
+++ src/database/database.sql
@@ -39,11 +39,11 @@ CREATE TABLE `users` (
     `firstname` VARCHAR(39) NOT NULL,
     `mail` VARCHAR(150) NOT NULL,
     `password` VARCHAR(50) NOT NULL,
-    `permissions_id` INT(10) UNSIGNED NOT NULL,
+    `permission_id` INT(10) UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE(mail),
-    KEY `FK_users_permissions` (`permissions_id`),
-    CONSTRAINT `FK_users_permissions` FOREIGN KEY (`permissions_id`) REFERENCES `permissions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    KEY `FK_users_permissions` (`permission_id`),
+    CONSTRAINT `FK_users_permissions` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `recipes` ( 
@@ -54,45 +54,45 @@ CREATE TABLE `recipes` (
     `guests` TINYINT,
     `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
     `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), 
-    `courses_id` INT(10) UNSIGNED NOT NULL,
-    `seasons_id` INT(10) UNSIGNED,
-    `users_id` INT(10) UNSIGNED NOT NULL,
+    `course_id` INT(10) UNSIGNED NOT NULL,
+    `season_id` INT(10) UNSIGNED,
+    `user_id` INT(10) UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE(slug),
-    KEY `FK_recipes_courses` (`courses_id`),
-    CONSTRAINT `FK_recipes_courses` FOREIGN KEY (`courses_id`) REFERENCES `courses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    KEY `FK_recipes_seasons` (`seasons_id`),
-    CONSTRAINT `FK_recipes_seasons` FOREIGN KEY (`seasons_id`) REFERENCES `seasons` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    KEY `FK_recipes_users` (`users_id`),
-    CONSTRAINT `FK_recipes_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    KEY `FK_recipes_courses` (`course_id`),
+    CONSTRAINT `FK_recipes_courses` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    KEY `FK_recipes_seasons` (`season_id`),
+    CONSTRAINT `FK_recipes_seasons` FOREIGN KEY (`season_id`) REFERENCES `seasons` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    KEY `FK_recipes_users` (`user_id`),
+    CONSTRAINT `FK_recipes_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `steps` ( 
     `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, 
     `content` TEXT NOT NULL,
-    `recipes_id` INT(10) UNSIGNED NOT NULL,
+    `recipe_id` INT(10) UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
-    KEY `FK_steps_recipes` (`recipes_id`),
-    CONSTRAINT `FK_steps_recipes` FOREIGN KEY (`recipes_id`) REFERENCES `recipes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    KEY `FK_steps_recipes` (`recipe_id`),
+    CONSTRAINT `FK_steps_recipes` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `images` ( 
     `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     `url` VARCHAR(250) NOT NULL,
     `alternate_text` VARCHAR(200),
-    `recipes_id` INT(10) UNSIGNED NOT NULL,
+    `recipe_id` INT(10) UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
-    KEY `FK_images_recipes` (`recipes_id`),
-    CONSTRAINT `FK_images_recipes` FOREIGN KEY (`recipes_id`) REFERENCES `recipes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    KEY `FK_images_recipes` (`recipe_id`),
+    CONSTRAINT `FK_images_recipes` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ingredients_recipes` (
-    `id` INT(10) UNSIGNED NOT NULL, 
-    `recipes_id` INT(10) UNSIGNED NOT NULL,
+    `ingredient_id` INT(10) UNSIGNED NOT NULL, 
+    `recipe_id` INT(10) UNSIGNED NOT NULL,
     `quantity` DOUBLE NOT NULL,
     PRIMARY KEY (`id`, `recipes_id`),
-    KEY `FK_ingredients_recipes_ingredients` (`id`),
-    KEY `FK_ingredients_recipes_recipes` (`recipes_id`),
-    CONSTRAINT `FK_ingredients_recipes_ingredients` FOREIGN KEY (`id`) REFERENCES `ingredients` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `FK_ingredients_recipes_recipes` FOREIGN KEY (`recipes_id`) REFERENCES `recipes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    KEY `FK_ingredients_recipes_ingredients` (`ingredient_id`),
+    KEY `FK_ingredients_recipes_recipes` (`recipe_id`),
+    CONSTRAINT `FK_ingredients_recipes_ingredients` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `FK_ingredients_recipes_recipes` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
