e3f58cf5c7c6fa4d3d0d3df60273cfadcd61da21
Ewen <ewen.herve.35550@gmail.com>
2022-10-15 18:42:20 +0200

    refactor: update index

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const constants_1 = require("./config/constants");
+const Recipe_1 = require("./routes/recipes/Recipe");
+const User_1 = require("./routes/users/User");
+const jwt_1 = require("./authentication/jwt");
 const app = (0, express_1.default)();
+const allowedOrigins = ['http://localhost:3000'];
+const options = {
+    origin: allowedOrigins
+};
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+console.log("Le token JWT : ", (0, jwt_1.generateToken)());
 app.get("/", (req, res) => res.send("Hello world !"));
+// Recipes routes
+app.get("/recipes", Recipe_1.router);
+app.get("/recipe/show/:id", Recipe_1.router);
+// Users routes
+app.get("/users", User_1.router);
+app.get("/user/:id", User_1.router);
 app.listen(constants_1.PORT, () => {
     console.log(`Server is listening on port ${constants_1.PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 require('dotenv').config();
 
 import express from 'express';
+import cors from 'cors';
 import { PORT } from './config/constants';
+import { router as recipeRouter } from './routes/recipes/Recipe';
+import { router as userRouter } from './routes/users/User';
+import { generateToken } from './authentication/jwt';
 
 const app = express();
+const allowedOrigins = ['http://localhost:3000'];
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+};
+app.use(cors());
 app.use(express.json());
 
+console.log("Le token JWT : ", generateToken());
+
 app.get("/", (req, res) => res.send("Hello world !"));
+
+// Recipes routes
+app.get("/recipes", recipeRouter);
+app.get("/recipe/show/:id", recipeRouter);
+
+// Users routes
+app.get("/users", userRouter);
+app.get("/user/:id", userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
