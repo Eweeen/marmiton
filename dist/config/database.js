4615438a2b84430658dffa2d2778f0b74c10da82
Ewen <ewen.herve.35550@gmail.com>
2022-10-15 18:41:39 +0200

    feat: add database connection

---
+++ dist/config/database.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sequelize = void 0;
+const sequelize_1 = require("sequelize");
+exports.sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+});
