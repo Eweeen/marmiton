8fc5d2e95fb321cf820d988ba73da38b51b82295
Ewen <ewen.herve.35550@gmail.com>
2022-10-17 11:09:02 +0200

    fix: warning useUTC

--- dist/config/database.js
+++ dist/config/database.js
@@ -5,8 +5,5 @@ const sequelize_1 = require("sequelize");
 exports.sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    dialectOptions: {
-        useUTC: false
-    },
     timezone: '+02:00'
 });
--- src/config/database.ts
+++ src/config/database.ts
@@ -3,8 +3,5 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USERNAME!, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    dialectOptions: {
-        useUTC: false
-    },
     timezone: '+02:00'
 });
