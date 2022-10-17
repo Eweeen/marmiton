79d4d55f4d2ed88e805c98497f4c072e2f50ec56
Ewen <ewen.herve.35550@gmail.com>
2022-10-17 09:51:37 +0200

    fix: timezone

--- dist/config/database.js
+++ dist/config/database.js
@@ -4,5 +4,9 @@ exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
 exports.sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+        useUTC: false
+    },
+    timezone: '+02:00'
 });
--- src/config/database.ts
+++ src/config/database.ts
@@ -2,5 +2,9 @@ import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USERNAME!, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+        useUTC: false
+    },
+    timezone: '+02:00'
 });
