26d4e626399a2c3a2476b9496addf405ee3b292c
Ewen <ewen.herve.35550@gmail.com>
2022-10-17 16:12:00 +0200

    feat: authentication

---
+++ dist/controllers/AuthenticationController.js
@@ -0,0 +1,51 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthenticationController = void 0;
+const jwt_1 = require("../authentication/jwt");
+const Permission_1 = require("../models/Permission");
+const User_1 = require("../models/User");
+const CrudController_1 = require("./CrudController");
+const bcrypt_1 = require("bcrypt");
+const constants_1 = require("../config/constants");
+class AuthenticationController extends CrudController_1.CrudController {
+    async signin(req, res) {
+        let user = req.body;
+        (0, bcrypt_1.hash)(user.password, constants_1.BCRYPT_ROUND)
+            .then(hashPass => {
+            user.password = hashPass;
+            User_1.User.create(user, { fields: ['firstname', 'lastname', 'password', 'mail'] })
+                .then(() => res.json("User create successful"))
+                .catch(error => {
+                console.log(error);
+                res.json({ message: "Cannot create user" });
+            });
+        })
+            .catch(error => {
+            console.log(error);
+            res.json({ message: "Cannot create user" });
+        });
+    }
+    ;
+    async login(req, res) {
+        const user = await User_1.User.findOne({
+            where: { mail: req.body.mail },
+            include: {
+                model: Permission_1.Permission,
+                as: 'permission'
+            }
+        });
+        if (user) {
+            if (await (0, bcrypt_1.compare)(req.body.password, user.password)) {
+                const token = (0, jwt_1.generateToken)(user);
+                res.json({ token: token });
+            }
+            else {
+                res.json({ message: "Email or password incorrect" });
+            }
+        }
+        else {
+            res.json({ message: "Email or password incorrect" });
+        }
+    }
+}
+exports.AuthenticationController = AuthenticationController;
---
+++ src/controllers/AuthenticationController.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { generateToken } from "../authentication/jwt";
+import { Permission } from "../models/Permission";
+import { User } from "../models/User";
+import { CrudController } from "./CrudController";
+import { hash, compare } from 'bcrypt';
+import { BCRYPT_ROUND } from "../config/constants";
+
+export class AuthenticationController extends CrudController
+{
+    public async signin(req: Request, res: Response): Promise<void> {
+        let user = req.body;
+        hash(user.password, BCRYPT_ROUND)
+        .then(hashPass => {
+            user.password = hashPass;
+
+            User.create(user, { fields: ['firstname', 'lastname', 'password', 'mail'] })
+            .then(() => res.json("User create successful"))
+            .catch(error => {
+                console.log(error);
+                res.json({ message: "Cannot create user" });
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            res.json({ message: "Cannot create user" });
+        });
+    };
+
+    public async login(req: Request, res: Response): Promise<void> {
+        const user = await User.findOne({
+            where: { mail: req.body.mail },
+            include: {
+                model: Permission,
+                as: 'permission'
+            }
+        });
+
+        if (user) {
+            if (await compare(req.body.password, user.password)) {
+                const token = generateToken(user);
+                res.json({ token: token });
+            } else {
+                res.json({ message: "Email or password incorrect" });
+            }
+        } else {
+            res.json({ message: "Email or password incorrect" });
+        }
+    }
+}
